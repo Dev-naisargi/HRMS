cb1966ecdd505832866506e34f47d3e3945d067d
Dev-naisargi <naissargibhatt@gmail.com>
2026-01-27 15:32:30 +0530

    created folder structure

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Login clicked");
+    // UI only – no authentication logic
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white border-1-4 border-blue-600 rounded-xl shadow-lg  p-8  w-full max-w-sm"
+      >
+        <h2 className="text-2xl font-semibold text-center mb-6">
+          HRMS Login
+        </h2>
+
+        <div className="mb-4">
+          <label className="block text-gray-600 mb-1">Email</label>
+          <input
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="w-full px-3 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+            required
+          />
+        </div>
+
+        <div className="mb-6">
+          <label className="block text-gray-600 mb-1">Password</label>
+          <input
+            type="password"
+            placeholder="Enter password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="w-full px-3 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+            required
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="w-full bg-blue-600 text-white py-2 rounded hover:bg-blue-700 transition"
+        >
+          Login
+        </button>
+      </form>
+    </div>
+  );
+}
